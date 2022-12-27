b83a588828f87332fe35851b2c8d6be0f82174dc
Russell <iruss1337@gmail.com>
2022-12-27 11:31:49 +0800

    updated schedule.js

--- src/routes/schedule.js
+++ src/routes/schedule.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const schedule = require('node-schedule');
 
 router.post("/", async (req, res) => {
+    let job = req.body.name;
     let cleaning_date = req.body.cleaning_date.split("-");
     let cleaning_time = req.body.cleaning_time.split(":");
     let map_name = req.body.map_name;
@@ -11,9 +12,9 @@ router.post("/", async (req, res) => {
     // console.log(cleaning_time);
     const crontab_str = `${cleaning_time[1]} ${cleaning_time[0]} * * *`;
 
-    const job = schedule.scheduleJob(crontab_str, () => {
-        console.log("Came from UI")
-    });
+    // const job = schedule.scheduleJob(crontab_str, () => {
+    //     console.log("Came from UI")
+    // });
 
     res.json({ "message": crontab_str })
 });
