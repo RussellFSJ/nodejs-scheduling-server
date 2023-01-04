bd848c3a83bbacf7eead4b592d14ec1a5c5b1eb4
Russell <iruss1337@gmail.com>
2023-01-04 15:25:45 +0800

    modified addPosition and manageGoals

--- src/libs/ros/manageGoals.js
+++ src/libs/ros/manageGoals.js
@@ -1,25 +1,26 @@
 const path = require("path");
 const axios = require("axios");
 
-let url = path.join(process.env.API_URL, "position");
+let url = path.join(process.env.API_URL, "manage_goals");
 let response = {};
-let position = [];
+let success = false;
 
-const getPosition = async () => {
+const manageGoals = async (exec_code, argument) => {
     let request = {
         "api_key": process.env.API_KEY, "robot_name": process.env.ROBOT_NAME,
+        "exec_code": exec_code, "argument": argument
     };
 
     try {
         response = await axios.post(url, request);
 
-        position = response.data.result.pose2d;
+        success = response.data.success;
     }
     catch (error) {
         console.log(error);
     }
 
-    return position;
+    return success;
 }
 
-module.exports = getPosition;
+module.exports = manageGoals;       
--- src/models/schedules.js
+++ src/models/schedules.js
@@ -6,7 +6,8 @@ const mongoose = require("mongoose");
 // while schema can be left empty, you should define fields used in queries
 // https://stackoverflow.com/a/51860371/14372239
 const SchedulesSchema = new mongoose.Schema({
-    base_name: String
+    base_name: String,
+    schedule_id: String
 });
 
 // optional 3rd argument to remove ambiguity of collection name in database
