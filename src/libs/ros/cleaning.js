a6e362c6c60763949e4e30f61628a2667c4842c1
Russell <iruss1337@gmail.com>
2023-01-19 12:40:42 +0800

    modified createJob for schedule endpoints

--- src/libs/ros/cleaning.js
+++ src/libs/ros/cleaning.js
@@ -6,6 +6,7 @@ let response = {};
 let success = false;
 
 const cleaning = async (cleaning_plan, cleaning_zones) => {
+    
     let request = {
         "api_key": process.env.API_KEY, "robot_name": process.env.ROBOT_NAME,
         "process": cleaning_plan, "zones": cleaning_zones
