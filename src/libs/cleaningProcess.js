b81878785bcbc4be5748993984b33cfa8105536a
Russell <iruss1337@gmail.com>
2023-01-30 16:37:57 +0800

    saving progress

--- src/libs/cleaningProcess.js
+++ src/libs/cleaningProcess.js
@@ -5,73 +5,96 @@ const sleep = require("./sleep");
 
 // needs refinement for checks 
 const cleaningProcess = async (cleaning_plan, cleaning_zones) => {
-    let home_position = JSON.parse(process.env.HOME_ZONES)[cleaning_plan]
-    let robot_position = await getPosition();
+    try {
+        let message = "";
+        let get_result_count = 0;
+        let attempts = 0;
 
-    let docking_result = await getDockingResult();
+        let home_position = JSON.parse(process.env.HOME_ZONES)[cleaning_plan];
+        let robot_position = await getPosition();
 
-    // undock
-    docking("undock");
+        let docking_result = await getDockingResult();
 
-    // checks if undocking is successful 
-    while (!docking_result) {
-        console.log("Undocking...");
-        await sleep(1000);
-        docking_result = await getDockingResult();
-    }
+        // undock
+        docking("undock");
 
-    // localise
-    console.log("Localising...");
-    localise();
+        // checks if undocking is successful 
+        while (!docking_result) {
+            console.log("Undocking...");
+            await sleep(1000);
+            docking_result = await getDockingResult();
+            get_result_count += 1;
+            if (attempts >= 3) {
+                message += `Failed to undock after ${attempts} attempts.`;
+                throw message;
+            }
 
-    await sleep(10000);
+            if (!docking_result && get_result_count >= 10) {
+                docking("undock");
+                attempts += 1;
+                get_result_count = 0;
 
-    // starts cleaning_plan
-    await cleaning(cleaning_plan, cleaning_zones);
-    await sleep(10000);
-    console.log("Started cleaning plan.")
-
-    // checks goal_queue_size every 30s
-    while (await getGoalQueueSize() != 0) {
-        console.log("Cleaning...")
-        await sleep(5000);
-    }
-    
-    // navigate to home position
-    await navigate(home_position);
-
-    // checks goal queue when robot is navigating to home position
-    // sends the goal to home zone again if goal is cancelled before reaching home
-    while (euclidean_dist(robot_position.slice(0, 2), home_position.slice(0, 2)) >= 0.2) {
-        console.log("Navigating to home position...");
-
-        // navigate to home position
-        // await navigate(home_position);
-
-        if (await getGoalQueueSize() == 0) {
-            // add home position to end of cleaning plan again if cancelled 
-            publishGoal(home_position);
-            await manageGoals(0, "");
+                console.log(`Undocking retry attempt: ${attempts}`);
+            }
         }
 
-        await sleep(5000);
-        robot_position = await getPosition();
-    }
+        // localise
+        console.log("Localising...");
+        localise();
 
-    docking_result = await getDockingResult();
+        await sleep(10000);
 
-    // dock
-    docking("dock");
-    
-    // checks if docking is successful 
-    while (!docking_result) {
-        console.log("Docking...");
-        await sleep(1000);
+        // starts cleaning_plan
+        await cleaning(cleaning_plan, cleaning_zones);
+        await sleep(10000);
+        console.log("Started cleaning plan.")
+
+        // checks goal_queue_size every 30s
+        while (await getGoalQueueSize() != 0) {
+            console.log("Cleaning...")
+            await sleep(5000);
+        }
+
+        // navigate to home position
+        await navigate(home_position);
+
+        // checks goal queue when robot is navigating to home position
+        // sends the goal to home zone again if goal is cancelled before reaching home
+        while (euclidean_dist(robot_position.slice(0, 2), home_position.slice(0, 2)) >= 0.2) {
+            console.log("Navigating to home position...");
+
+            // navigate to home position
+            // await navigate(home_position);
+
+            if (await getGoalQueueSize() == 0) {
+                // add home position to end of cleaning plan again if cancelled 
+                publishGoal(home_position);
+                await manageGoals(0, "");
+            }
+
+            await sleep(5000);
+            robot_position = await getPosition();
+        }
+
         docking_result = await getDockingResult();
+
+        // dock
+        docking("dock");
+
+        // checks if docking is successful 
+        while (!docking_result) {
+            console.log("Docking...");
+            await sleep(1000);
+            docking_result = await getDockingResult();
+        }
+
+        console.log("Done.");
+
     }
-
-    console.log("Done.");
-
+    catch (error) {
+        message += error;
+        console.log(message);
+    }
 }
 
 module.exports = cleaningProcess;
